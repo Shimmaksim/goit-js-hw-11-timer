e958fc6a12e7335626c5e172abf1c8dd270b9374
shimmaksim <shimmaksim@gmail.com>
2019-11-17 21:39:53 +0200

    sjgbjs

---
+++ js/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+import { CountdownTimer } from "./timer.js";
+
+const timer = new CountdownTimer({
+  selector: "#timer-1",
+  targetDate: new Date("Nov 5, 2019")
+});
+
+timer.init();
